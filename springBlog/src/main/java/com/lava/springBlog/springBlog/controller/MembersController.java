cb9df49737cce94a265d189b8e2c226d4e500674
lavaspoon <lavaspoon@naver.com>
2021-12-16 01:38:14 +0900

    회원가입 폼 - 타임리프 값 가져오기 세팅

--- springBlog/src/main/java/com/lava/springBlog/springBlog/controller/MembersController.java
+++ springBlog/src/main/java/com/lava/springBlog/springBlog/controller/MembersController.java
@@ -4,6 +4,7 @@ import com.lava.springBlog.springBlog.model.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -13,13 +14,15 @@ public class MembersController {
     private static final Logger logger = LoggerFactory.getLogger(MembersController.class);
     //로그인 페이지
     @GetMapping("/addMember")
-    public String showPage(){
+    public String showPage(Model model){
+        //타임리프로 값을 불러오기 때문에 빈값을 넘겨줘야함
+        model.addAttribute("member", new Member());
         return "html/Members/addMember";
     }
 
     //Data 전달
     @RequestMapping(value = {"/addMember"}, method = {RequestMethod.POST})
-    public void addMember(@ModelAttribute Member member){
+    public String addMember(@ModelAttribute Member member, Model model){
         logger.info("=========> 회원가입 전송된 데이터 <========");
         logger.info("getUserID = {}", member.getUserID());
         logger.info("getUserPWD = {}", member.getUserPWD());
@@ -27,5 +30,8 @@ public class MembersController {
         logger.info("getUserName = {}", member.getUserName());
         logger.info("getUserPhone = {}", member.getUserPhone());
         logger.info("=====================================");
+
+        model.addAttribute("member", member);
+        return "html/Members/addMember";
     }
 }
