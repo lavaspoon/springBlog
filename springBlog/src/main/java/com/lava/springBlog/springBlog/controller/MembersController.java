a0425c122f7bb7727779ec1c9faba6e42603caca
lavaspoon <lavaspoon@naver.com>
2021-12-17 18:17:05 +0900

    회원가입 페이지 validation 처리 - JSP - Spring 완료

--- springBlog/src/main/java/com/lava/springBlog/springBlog/controller/MembersController.java
+++ springBlog/src/main/java/com/lava/springBlog/springBlog/controller/MembersController.java
@@ -6,12 +6,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *  validate
+ *  JSP 검증 O
+ *  Spring 검증 O
+ */
 @Controller
 @RequestMapping("/html/Members/")
 public class MembersController {
@@ -19,27 +23,20 @@ public class MembersController {
     //로그인 페이지
     @GetMapping("/addMember")
     public String showPage(Model model){
-        System.out.println("MembersController.showPage");
         //타임리프로 값을 불러오기 때문에 빈값을 넘겨줘야함
-        model.addAttribute("member", new Member());
+        //model.addAttribute("member", new Member());
         return "html/Members/addMember";
     }
 
     //Data 전달 - v1
     @ResponseBody
     @RequestMapping(value = {"/addMember"}, method = {RequestMethod.POST})
-    public String addMember(@Validated @ModelAttribute Member member, Model model){
-        logger.info("=========> 회원가입 전송된 데이터 <========");
-        logger.info("getUserID = {}", member.getUserID());
-        logger.info("getUserPWD = {}", member.getUserPWD());
-        logger.info("getUserPWD_Check = {}", member.getUserPWD_Check());
-        logger.info("getUserName = {}", member.getUserName());
-        logger.info("getUserPhone = {}", member.getUserPhone());
-        logger.info("=====================================");
-        model.addAttribute("member", member);
-        //valid 검증
+    public HashMap<String, String> addMember(@ModelAttribute Member member, Model model){
+
+        HashMap<String, String> msg = new HashMap<>();
         Map<String, String> errors = new HashMap<>();
 
+        //valid 검증
         if(!StringUtils.hasText(member.getUserID())){
             errors.put("ID_Error", "ID는 필수 값 입니다.");
         }
@@ -51,12 +48,12 @@ public class MembersController {
         }
 
         if(!errors.isEmpty()){
-            System.out.println("Fail");
             logger.info("errors = {}", errors);
-            model.addAttribute("errors", errors);
-            return "html/Members/addMember";
+            msg.put("message", "회원가입 실패");
+            return msg;
         }
-        System.out.println("Success");
-        return "redirect:/html/Members/addMember";
+
+        msg.put("message", "회원가입 성공");
+        return msg;
     }
 }
--- springBlog/src/main/resources/static/javascript/Members/addMember.js
+++ springBlog/src/main/resources/static/javascript/Members/addMember.js
@@ -1,14 +1,32 @@
 $('#Members-addMember-submit').on('click', function (){
     var userID = $('#userID').val();
+    var userPWD = $('#userPWD').val();
+    var userPWD_Check = $('#userPWD_Check').val();
+    var userName = $('#userName').val();
+    var userPhone = $('#userPhone').val();
+
     if(userID == ""){
-        $("#id_check").text("아이디를 입력해주세요");
+        $("#ID_check").text("아이디를 입력해주세요");
+    } else if (userPWD == "") {
+        $("#PWD_check").text("비밀번호를 입력해주세요");
+    } else if (userPWD_Check == "") {
+        $("#PWD_Check_check").text("비밀번호를 입력해주세요");
+    } else if (userPWD != userPWD_Check) {
+        $("#PWD_Check_check").text("비밀번호가 일치하지 않습니다");
     } else {
         $.ajax({
             url: "/html/Members/addMember",
             type: "POST",
-            data: $("#Members-addMember").serialize(),
+            data:
+                {
+                    userID : userID,
+                    userPWD : userPWD,
+                    userPWD_Check : userPWD_Check,
+                    userName : userName,
+                    userPhone : userPhone
+                },
             success: function (data){
-                alert(data);
+                alert(data.message);
             },
             error: function (){
                 alert("server error");
